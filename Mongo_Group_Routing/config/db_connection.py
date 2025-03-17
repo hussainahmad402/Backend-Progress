ed26dd0b9d2f6f79f22d7b55810a0911f7a68a53
hussainahmad402 <hussainahmad9402@gmail.com>
2025-03-17 13:45:03 +0500

    revert changes

--- Mongo_Group_Routing/config/db_connection.py
+++ Mongo_Group_Routing/config/db_connection.py
@@ -6,7 +6,7 @@ dotenv_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), ".env")
 load_dotenv(dotenv_path)
 
 db_url=os.getenv("DB_URL")
-
+ 
 def get_db():
     try:
         client=MongoClient(db_url)
